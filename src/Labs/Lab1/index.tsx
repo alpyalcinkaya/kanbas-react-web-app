560a501af8a9025e792b60e458e2855d8cf11235
Alp Yalcinkaya <yalcinkayaalp@gmail.com>
2024-09-17 17:20:08 -0400

    edit alt attributes

--- src/Labs/Lab1/index.tsx
+++ src/Labs/Lab1/index.tsx
@@ -125,12 +125,12 @@ export default function Lab1() {
             <img id="wd-starship"
                 width="400px"
             src="https://www.staradvertiser.com/wp-content/uploads/2021/08/web1_Starship-gap2.jpg"
-            alt="image of starship"
+            alt="starship"
             />
             <br />
             Loading a local image:
             <br />
-            <img id="wd-teslabot" src="images/teslabot.jpg" alt="image of teslabot" height="200px" />
+            <img id="wd-teslabot" src="images/teslabot.jpg" alt="teslabot" height="200px" />
         </div>
 
         <div id="wd-forms">
