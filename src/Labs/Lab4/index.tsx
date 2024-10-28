79d6f0ac7c7d269853cf0184238886858ac285dc
Alp Yalcinkaya <yalcinkayaalp@gmail.com>
2024-10-28 16:02:52 -0400

    Reach until the redux portion of a4

---
+++ src/Labs/Lab4/index.tsx
@@ -0,0 +1,41 @@
+import React from "react";
+import ClickEvent from "./ClickEvent";
+import PassingDataOnEvent from "./PassingDataOnEvent";
+import PassingFunctions from "./PassingFunctions";
+import EventObject from "./EventObject";
+import Counter from "./Counter";
+import BooleanStateVariables from "./BooleanStateVariables";
+import StringStateVariables from "./StringStateVariables";
+import DateStateVariable from "./DateStateVatiable";
+import ObjectStateVariable from "./ObjectStateVariable";
+import ArrayStateVariable from "./ArrayStateVariable";
+import ParentStateComponent from "./ParentStateComponent";
+import ReduxExamples from "./ReduxExamples";
+
+
+export default function Lab4(){
+    function sayHello() {
+        alert("Hello");
+      }
+    
+    return (
+    <div className="container" id="wd-lab4">
+        <h2>Lab 4 - Managing State in React.js</h2>
+
+        <ClickEvent />
+        <PassingDataOnEvent />
+        <PassingFunctions theFunction={sayHello}/>
+        <EventObject />
+        <Counter />
+        <BooleanStateVariables />
+        <StringStateVariables />
+        <DateStateVariable />
+        <ObjectStateVariable />
+        <ArrayStateVariable />
+        <ParentStateComponent />
+        <ReduxExamples />
+        
+    </div>
+    
+    )
+}
