80dc6c62939f04353dd06f27606bf3a7fa2698e9
Maggy <chrismegaman@live.com>
2024-11-27 14:12:51 -0500

    functions should be done, goign tof ront end now

--- src/Kanbas/Courses/Quizzes/EditorScreen.tsx
+++ src/Kanbas/Courses/Quizzes/EditorScreen.tsx
@@ -4,6 +4,8 @@ import { useDispatch } from "react-redux";
 
 import * as quizClient from "./client";
 import { addQuiz, updateQuiz, deleteQuizAction, setQuizzes } from "./reducer";
+import { scheduler } from "timers/promises";
+import { BiSolidPhoneOutgoing } from "react-icons/bi";
 
 
 export default function QuizEdirot() {
@@ -22,7 +24,6 @@ export default function QuizEdirot() {
   const [title, setTitle] = useState("");
   const [description, setDescription] = useState("");
   const [points, setPoints] = useState("100");
-  const [shuffleAnswers, setShuffle] = useState("Yes");
   const [timeLimit, setTimeLimit] = useState("20");
   const [accesCode, setAccesCode] = useState("");
   const [dueDate, setDueDate] = useState("");
@@ -32,6 +33,7 @@ export default function QuizEdirot() {
   // check box
   const [multipleAttempts, setAttempts] = useState(false);
   const [numberAttempts, setNumberOfAttempts] = useState("1");
+  const [shuffleAnswers, setShuffle] = useState(true);
   const [showCorrectAnswers, setShowAnswer] = useState(false);
   const [oneAtATime, setOneAtATime] = useState(true);
   const [webCam, setWebCam] = useState(false);
@@ -50,33 +52,72 @@ export default function QuizEdirot() {
       if (isEditing && aid) {
         const quiz = await quizClient.findQuizzesForCourse(cid!);
         if (quiz) {
-          setTitle(quiz.title);
-          setDescription(quiz.description);
-          setPoints(quiz.points);
-          setShuffle(quiz.shuffleAnswers);
-          setDueDate(quiz.dueDate);
-          setAvailableFromDate(quiz.availableDate);
-          setUntilDate(quiz.untilDate);
-          setTimeLimit(quiz.timeLimit);
-          setAccesCode(quiz.accesCode);
+          // set state with fetched data if any exists, else is blank as if new
+          // if the quiz is set with null values then set to blank/default state
+          setTitle(quiz.title || "");
+          setDescription(quiz.description || "");
+          setPoints(quiz.points?.toString() || "");
+          setDueDate(quiz.dueDate || "");
+          setAvailableFromDate(quiz.availableDate || "");
+          setUntilDate(quiz.untilDate || "");
+          setTimeLimit(quiz.timeLimit || "20");
+          setAccesCode(quiz.accesCode || "");
 
-          setAttempts(quiz.multipleAttempts);
-          setNumberOfAttempts(quiz.numberAttempts);
-          setShowAnswer(quiz.showCorrectAnswers);
-          setOneAtATime(quiz.oneAtATime);
-          setWebCam(quiz.webCam);
-          setLockQuestions(quiz.lockQuestionsAfterAnswering);
+          setAttempts(quiz.multipleAttempts || false);
+          setNumberOfAttempts(quiz.numberAttempts || "1");
+          setShuffle(quiz.shuffleAnswers || true);
+          setShowAnswer(quiz.showCorrectAnswers || false);
+          setOneAtATime(quiz.oneAtATime || true);
+          setWebCam(quiz.webCam || false);
+          setLockQuestions(quiz.lockQuestionsAfterAnswering || false);
 
-          setAssignmentGroup(quiz.assignmentGroup);
-          setDisplayGradeAs(quiz.displayGradeAs);
-          setSubmissionType(quiz.submissionType);
-          setQuizType(quiz.quizType);
+          setAssignmentGroup(quiz.assignmentGroup || "QUIZZES");
+          setDisplayGradeAs(quiz.displayGradeAs || "PERCENTAGE");
+          setSubmissionType(quiz.submissionType || "ONLINE");
+          setQuizType(quiz.quizType || "Graded Quiz");
         }
       }
     };
     fetchAssignment();
   }, [cid, aid]);
 
+  const handleSave = async () => {
+    const updatedQuiz = {
+      _id: isEditing ? aid! : new Date().getTime().toString(),
+      title, 
+      course: cid, // might need to be string 
+      description,
+      points: parseInt(points),
+      dueDate,
+      availableDate,
+      untilDate, 
+      timeLimit,
+      accesCode,
+      multipleAttempts,
+      numberAttempts,
+      shuffleAnswers,
+      showCorrectAnswers,
+      oneAtATime,
+      webCam,
+      lockQuestionsAfterAnswering,
+      assignmentGroup,
+      displayGradeAs,
+      submissionType,
+      quizType,
+
+    };
+
+    if (isEditing) {
+      await quizClient.updateQuiz( updatedQuiz);
+      dispatch(updateQuiz(updatedQuiz));
+    } else {
+      const newQuiz = await quizClient.createQuizForCourse(cid!, updatedQuiz);
+      dispatch(addQuiz(newQuiz));
+
+    }
+
+    navigate(`/Kanbas/Courses/${cid}/Quizzes`);
+  }
 
 
 
