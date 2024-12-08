68adcb20d38d1f36c4905326d610366ea90b6c6e
Alp Yalcinkaya <yalcinkayaalp@gmail.com>
2024-12-08 17:34:16 -0500

    try debgging faculty view

--- src/Kanbas/Courses/client.ts
+++ src/Kanbas/Courses/client.ts
@@ -2,38 +2,40 @@ import axios from "axios";
 const REMOTE_SERVER = process.env.REACT_APP_REMOTE_SERVER;
 const COURSES_API = `${REMOTE_SERVER}/api/courses`;
 
+const axiosWithCredentials = axios.create({ withCredentials: true });
+
 // Fetch all courses
 export const fetchAllCourses = async () => {
-  const { data } = await axios.get(COURSES_API);
+  const { data } = await axiosWithCredentials.get(COURSES_API);
   return data;
 };
 
 // Fetch courses managed by a specific user (faculty)
 export const findCoursesManagedByUser = async (userId: string) => {
-  const { data } = await axios.get(`${COURSES_API}/managed/${userId}`);
+  const { data } = await axiosWithCredentials.get(`${COURSES_API}/managed/${userId}`);
   return data;
 };
 
 // Delete a course
 export const deleteCourse = async (id: string) => {
-  const { data } = await axios.delete(`${COURSES_API}/${id}`);
+  const { data } = await axiosWithCredentials.delete(`${COURSES_API}/${id}`);
   return data;
 };
 
 // Update a course
 export const updateCourse = async (course: any) => {
-  const { data } = await axios.put(`${COURSES_API}/${course._id}`, course);
+  const { data } = await axiosWithCredentials.put(`${COURSES_API}/${course._id}`, course);
   return data;
 };
 
 // Fetch modules for a specific course
 export const findModulesForCourse = async (courseId: string) => {
-  const response = await axios.get(`${COURSES_API}/${courseId}/modules`);
+  const response = await axiosWithCredentials.get(`${COURSES_API}/${courseId}/modules`);
   return response.data;
 };
 
 // Create a module for a specific course
 export const createModuleForCourse = async (courseId: string, module: any) => {
-  const response = await axios.post(`${COURSES_API}/${courseId}/modules`, module);
+  const response = await axiosWithCredentials.post(`${COURSES_API}/${courseId}/modules`, module);
   return response.data;
 };
